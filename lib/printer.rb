da48402a44a1bf707e70f20107239624c680b4e4
Jason Wright <noobjey@gmail.com>
2015-05-24 16:56:11 -0600

    second test and refactoring

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,5 +1,15 @@
 class Printer
-  def jagged_output
-    'Nothing to print!'
+  def jagged_output(values)
+    output = 'Nothing to print!'
+
+    return output unless values.is_a? Array
+
+    output = ''
+
+    values.each do |value|
+      output += "#{value} "
+    end
+
+    output.rstrip
   end
 end
--- spec/lib/printer_spec.rb
+++ spec/lib/printer_spec.rb
@@ -1,10 +1,27 @@
 require './lib/printer'
 
 describe 'printer.jagged_output' do
-
-  it 'should print nothing when no input' do
+  it 'should print nothing when not an array' do
     printer = Printer.new
+    value = 'not an array'
+    output  = 'Nothing to print!'
 
-    expect(printer.jagged_output).to eq('Nothing to print!')
+    expect(printer.jagged_output(value)).to eq(output)
+  end
+
+  it 'should print one value when one value' do
+    printer = Printer.new
+    value = [0]
+    output  = '0'
+
+    expect(printer.jagged_output(value)).to eq(output)
+  end
+
+  it 'should print two values when two values' do
+    printer = Printer.new
+    values = [0, 1]
+    output  = '0 1'
+
+    expect(printer.jagged_output(values)).to eq(output)
   end
 end
