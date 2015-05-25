3c3d5fd6d2184a310f4986dfc37c1ae53f4704b5
Jason Wright <noobjey@gmail.com>
2015-05-24 23:40:57 -0600

    removed some comments

--- lib/fibber.rb
+++ lib/fibber.rb
@@ -1,14 +1,3 @@
-# The sequence begins with the numbers 0 and 1. From there, each element is the sum of the previous
-# two elements.
-# Start with 0 and 1
-# 0 plus 1 is (1)
-# 1 plus 1 is (2)
-# 1 plus 2 is (3)
-# 2 plus 3 is (5)
-# 3 plus 5 is (8)
-# 5 plus 8 is (13)
-# 8 plus 13 is (21)
-
 class Fibber
   def self.generate(quantity)
     if (quantity.is_a? String) || (quantity < 1)
